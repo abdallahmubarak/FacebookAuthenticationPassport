0a9e423b123b1d5cc06328fa63d618cf6bda08f9
abdallah <abdallahmubarak55@gmail.com>
2022-09-08 04:13:55 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'facebookAuth': {
+    'clientID': '479287004055158', // your App ID
+    'clientSecret': '93009330510d76f34197485d971a733c', // your App Secret
+    'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+  }
+}
+
